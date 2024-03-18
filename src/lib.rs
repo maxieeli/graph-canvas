72df80c9bb58b8dc22a667977024459df5d1f142
maxiee <owenlideveloper@gmail.com>
2024-03-19 07:52:04 +0800

    feat: update lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,539 @@
+#![feature(link_cfg)]
+#![feature(let_chains)]
 #![deny(clippy::all)]
+#![allow(clippy::many_single_char_names)]
+#![allow(clippy::too_many_arguments)]
+#![allow(clippy::new_without_default)]
 
 #[macro_use]
 extern crate napi_derive;
+#[macro_use]
+extern crate serde_derive;
+
+use std::str::FromStr;
+use std::{mem, slice};
+
+use base64::Engine;
+use napi::bindgen_prelude::{AsyncTask, ClassInstance, Either3, This, Unknown};
+use napi::*;
+
+use ctx::{
+  CanvasRenderingContext2D, Context, ContextData, ContextOutputData, SvgExportFlag,
+  FILL_STYLE_HIDDEN_NAME, STROKE_STYLE_HIDDEN_NAME,
+};
+use font::{init_font_regexp, FONT_REGEXP};
+use sk::{ColorSpace, SkiaDataRef};
+
+use avif::AvifConfig;
+
+#[global_allocator]
+static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+mod avif;
+mod ctx;
+mod error;
+mod filter;
+mod font;
+pub mod global_fonts;
+mod gradient;
+mod image;
+pub mod path;
+mod pattern;
+pub mod picture_recorder;
+#[allow(dead_code)]
+mod sk;
+mod state;
+pub mod svg;
+
+const MIME_WEBP: &str = "image/webp";
+const MIME_PNG: &str = "image/png";
+const MIME_JPEG: &str = "image/jpeg";
+const MIME_AVIF: &str = "image/avif";
+
+// Consistent with the default value of JPEG quality in Blink
+// https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/image-encoders/image_encoder.cc;l=85;drc=81c6f843fdfd8ef660d733289a7a32abe68e247a
+const DEFAULT_JPEG_QUALITY: u8 = 92;
+
+// Consistent with the default value of WebP quality in Blink
+// https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/image-encoders/image_encoder.cc;l=100;drc=81c6f843fdfd8ef660d733289a7a32abe68e247a
+const DEFAULT_WEBP_QUALITY: u8 = 80;
+
+#[napi::module_init]
+fn init() {
+  // pre init font regexp
+  FONT_REGEXP.get_or_init(init_font_regexp);
+}
+
+#[napi(object)]
+pub struct CanvasRenderingContext2DAttributes {
+  pub alpha: Option<bool>,
+  pub color_space: Option<String>,
+}
 
 #[napi]
-pub fn sum(a: i32, b: i32) -> i32 {
-  a + b
+pub struct CanvasElement {
+  pub(crate) width: u32,
+  pub(crate) height: u32,
+  pub(crate) ctx: ClassInstance<CanvasRenderingContext2D>,
+}
+
+#[napi]
+impl CanvasElement {
+  fn create_context(
+    mut env: Env,
+    width: u32,
+    height: u32,
+  ) -> Result<ClassInstance<CanvasRenderingContext2D>> {
+    let ctx = CanvasRenderingContext2D {
+      context: Context::new(width, height, ColorSpace::default())?,
+    }
+    .into_instance(env)?;
+    ctx.as_object(env).define_properties(&[
+      Property::new(FILL_STYLE_HIDDEN_NAME)?
+        .with_value(&env.create_string("#000")?)
+        .with_property_attributes(PropertyAttributes::Writable | PropertyAttributes::Configurable),
+      Property::new(STROKE_STYLE_HIDDEN_NAME)?
+        .with_value(&env.create_string("#000")?)
+        .with_property_attributes(PropertyAttributes::Writable | PropertyAttributes::Configurable),
+    ])?;
+    env.adjust_external_memory((width * height * 4) as i64)?;
+    Ok(ctx)
+  }
+
+  #[napi(constructor)]
+  pub fn new(env: Env, mut this: This, width: u32, height: u32) -> Result<Self> {
+    let ctx = Self::create_context(env, width, height)?;
+    this.define_properties(&[Property::new("ctx")?
+      .with_value(&ctx)
+      .with_property_attributes(PropertyAttributes::Default)])?;
+    ctx
+      .as_object(env)
+      .define_properties(&[Property::new("canvas")?
+        .with_value(&this)
+        .with_property_attributes(
+          PropertyAttributes::Default
+            | PropertyAttributes::Writable
+            | PropertyAttributes::Enumerable,
+        )])?;
+    Ok(Self { width, height, ctx })
+  }
+
+  #[napi(setter)]
+  pub fn set_width(&mut self, mut env: Env, width: u32) -> Result<()> {
+    self.width = width;
+    let height = self.height;
+    let old_ctx = mem::replace(
+      &mut self.ctx.context,
+      Context::new(width, height, ColorSpace::default())?,
+    );
+    env.adjust_external_memory((width as i64 - old_ctx.width as i64) * 4)?;
+    Ok(())
+  }
+
+  #[napi(getter)]
+  pub fn get_width(&self) -> u32 {
+    self.width
+  }
+
+  #[napi(setter)]
+  pub fn set_height(&mut self, mut env: Env, height: u32) -> Result<()> {
+    self.height = height;
+    let width = self.width;
+    let old_ctx = mem::replace(
+      &mut self.ctx.context,
+      Context::new(width, height, ColorSpace::default())?,
+    );
+    env.adjust_external_memory((height as i64 - old_ctx.height as i64) * 4)?;
+    Ok(())
+  }
+
+  #[napi(getter)]
+  pub fn get_height(&self) -> u32 {
+    self.height
+  }
+
+  #[napi]
+  pub fn get_context(
+    &mut self,
+    this: This,
+    context_type: String,
+    attrs: Option<CanvasRenderingContext2DAttributes>,
+  ) -> Result<Unknown> {
+    if context_type != "2d" {
+      return Err(Error::new(
+        Status::InvalidArg,
+        format!("{context_type} is not supported"),
+      ));
+    }
+    let context_2d = &mut self.ctx.context;
+    if !attrs.as_ref().and_then(|a| a.alpha).unwrap_or(true) {
+      let mut fill_paint = context_2d.fill_paint()?;
+      fill_paint.set_color(255, 255, 255, 255);
+      context_2d.alpha = false;
+      context_2d.surface.draw_rect(
+        0f32,
+        0f32,
+        self.width as f32,
+        self.height as f32,
+        &fill_paint,
+      );
+    }
+    let color_space = attrs
+      .and_then(|a| a.color_space)
+      .and_then(|cs| ColorSpace::from_str(&cs).ok())
+      .unwrap_or_default();
+    context_2d.color_space = color_space;
+    this.get_named_property("ctx")
+  }
+
+  #[napi]
+  pub fn encode(
+    &self,
+    format: String,
+    quality_or_config: Either3<u32, AvifConfig, Unknown>,
+  ) -> Result<AsyncTask<ContextData>> {
+    Ok(AsyncTask::new(
+      self.encode_inner(format, quality_or_config)?,
+    ))
+  }
+
+  #[napi]
+  pub fn encode_sync(
+    &self,
+    env: Env,
+    format: String,
+    quality_or_config: Either3<u32, AvifConfig, Unknown>,
+  ) -> Result<JsBuffer> {
+    let mut task = self.encode_inner(format, quality_or_config)?;
+    let output = task.compute()?;
+    task.resolve(env, output)
+  }
+
+  #[napi]
+  pub fn to_buffer(
+    &self,
+    env: Env,
+    mime: String,
+    quality_or_config: Either3<u32, AvifConfig, Unknown>,
+  ) -> Result<JsBuffer> {
+    let mime = mime.as_str();
+    let context_data = get_data_ref(&self.ctx.context, mime, &quality_or_config)?;
+    match context_data {
+      ContextOutputData::Skia(data_ref) => unsafe {
+        env
+          .create_buffer_with_borrowed_data(
+            data_ref.0.ptr,
+            data_ref.0.size,
+            data_ref,
+            |data: SkiaDataRef, _| mem::drop(data),
+          )
+          .map(|b| b.into_raw())
+      },
+      ContextOutputData::Avif(output) => unsafe {
+        env
+          .create_buffer_with_borrowed_data(
+            output.as_ptr().cast_mut(),
+            output.len(),
+            output,
+            |data, _| mem::drop(data),
+          )
+          .map(|b| b.into_raw())
+      },
+    }
+  }
+
+  #[napi]
+  pub fn data(&self, env: Env) -> Result<JsBuffer> {
+    let ctx2d = &self.ctx.context;
+
+    let surface_ref = ctx2d.surface.reference();
+
+    let (ptr, size) = surface_ref.data().ok_or_else(|| {
+      Error::new(
+        Status::GenericFailure,
+        "Get png data from surface failed".to_string(),
+      )
+    })?;
+    unsafe {
+      env
+        .create_buffer_with_borrowed_data(ptr.cast_mut(), size, 0, noop_finalize)
+        .map(|value| value.into_raw())
+    }
+  }
+
+  #[napi(js_name = "toDataURLAsync")]
+  pub fn to_data_url_async(
+    &self,
+    mime: Option<String>,
+    quality_or_config: Either3<f64, AvifConfig, Unknown>,
+  ) -> Result<AsyncTask<AsyncDataUrl>> {
+    Ok(AsyncTask::new(
+      self.to_data_url_inner(mime.as_deref(), quality_or_config)?,
+    ))
+  }
+
+  #[napi(js_name = "toDataURL")]
+  pub fn to_data_url(
+    &self,
+    mime: Option<String>,
+    quality_or_config: Either3<f64, AvifConfig, Unknown>,
+  ) -> Result<String> {
+    let mut task = self.to_data_url_inner(mime.as_deref(), quality_or_config)?;
+    task.compute()
+  }
+
+  #[napi]
+  pub fn save_png(&self, path: String) {
+    let ctx2d = &self.ctx.context;
+    ctx2d.surface.save_png(&path);
+  }
+
+  fn encode_inner(
+    &self,
+    format: String,
+    quality_or_config: Either3<u32, AvifConfig, Unknown>,
+  ) -> Result<ContextData> {
+    let format_str = format.as_str();
+    let quality = quality_or_config.to_quality(format_str);
+    let ctx2d = &self.ctx.context;
+    let surface_ref = ctx2d.surface.reference();
+
+    let task = match format_str {
+      "webp" => ContextData::Webp(surface_ref, quality),
+      "jpeg" => ContextData::Jpeg(surface_ref, quality),
+      "png" => ContextData::Png(surface_ref),
+      "avif" => {
+        let cfg = AvifConfig::from(&quality_or_config);
+        ContextData::Avif(surface_ref, cfg.into(), ctx2d.width, ctx2d.height)
+      }
+      _ => {
+        return Err(Error::new(
+          Status::InvalidArg,
+          format!("{format_str} is not valid format"),
+        ))
+      }
+    };
+
+    Ok(task)
+  }
+
+  fn to_data_url_inner(
+    &self,
+    mime: Option<&str>,
+    quality_or_config: Either3<f64, AvifConfig, Unknown>,
+  ) -> Result<AsyncDataUrl> {
+    let mime = mime.unwrap_or(MIME_PNG);
+    let data_ref = get_data_ref(
+      &self.ctx.context,
+      mime,
+      &match quality_or_config {
+        Either3::A(q) => Either3::A((q * 100.0) as u32),
+        Either3::B(s) => Either3::B(s),
+        Either3::C(u) => Either3::C(u),
+      },
+    )?;
+    Ok(AsyncDataUrl {
+      surface_data: data_ref,
+      mime: mime.to_owned(),
+    })
+  }
+}
+
+#[napi(object)]
+pub struct ContextAttr {
+  pub alpha: Option<bool>,
+}
+
+fn get_data_ref(
+  ctx2d: &Context,
+  mime: &str,
+  quality_or_config: &Either3<u32, AvifConfig, Unknown>,
+) -> Result<ContextOutputData> {
+  let surface_ref = ctx2d.surface.reference();
+  let quality = quality_or_config.to_quality(mime);
+
+  if let Some(data_ref) = match mime {
+    MIME_WEBP => surface_ref.encode_data(sk::SkEncodedImageFormat::Webp, quality),
+    MIME_JPEG => surface_ref.encode_data(sk::SkEncodedImageFormat::Jpeg, quality),
+    MIME_PNG => surface_ref.png_data(),
+    MIME_AVIF => {
+      let (data, size) = surface_ref.data().ok_or_else(|| {
+        Error::new(
+          Status::GenericFailure,
+          "Encode to avif error, failed to get surface pixels".to_owned(),
+        )
+      })?;
+      let config = AvifConfig::from(quality_or_config).into();
+      let output = avif::encode(
+        unsafe { slice::from_raw_parts(data, size) },
+        ctx2d.width,
+        ctx2d.height,
+        &config,
+      )
+      .map_err(|e| Error::new(Status::GenericFailure, format!("{e}")))?;
+      return Ok(ContextOutputData::Avif(output));
+    }
+    _ => {
+      return Err(Error::new(
+        Status::InvalidArg,
+        format!("{mime} is not valid mime"),
+      ))
+    }
+  } {
+    Ok(ContextOutputData::Skia(data_ref))
+  } else {
+    Err(Error::new(
+      Status::InvalidArg,
+      format!("encode {mime} output failed"),
+    ))
+  }
+}
+
+pub struct AsyncDataUrl {
+  surface_data: ContextOutputData,
+  mime: String,
+}
+
+#[napi]
+impl Task for AsyncDataUrl {
+  type Output = String;
+  type JsValue = String;
+
+  fn compute(&mut self) -> Result<Self::Output> {
+    let mut output = format!("data:{};base64,", &self.mime);
+    match &self.surface_data {
+      ContextOutputData::Skia(data_ref) => {
+        base64::engine::general_purpose::STANDARD.encode_string(data_ref.slice(), &mut output);
+      }
+      ContextOutputData::Avif(data_ref) => {
+        base64::engine::general_purpose::STANDARD.encode_string(data_ref.as_ref(), &mut output);
+      }
+    }
+    Ok(output)
+  }
+
+  fn resolve(&mut self, _env: Env, output: Self::Output) -> Result<Self::JsValue> {
+    Ok(output)
+  }
+}
+
+trait ToQuality {
+  fn to_quality(&self, mime: &str) -> u8;
+}
+
+impl ToQuality for &Either3<u32, AvifConfig, Unknown> {
+  fn to_quality(&self, mime_or_format: &str) -> u8 {
+    if let Either3::A(q) = &self {
+      *q as u8
+    } else {
+      match mime_or_format {
+        MIME_WEBP | "webp" => DEFAULT_WEBP_QUALITY,
+        _ => DEFAULT_JPEG_QUALITY, // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL
+      }
+    }
+  }
+}
+
+impl ToQuality for Either3<u32, AvifConfig, Unknown> {
+  fn to_quality(&self, mime: &str) -> u8 {
+    ToQuality::to_quality(&self, mime)
+  }
+}
+
+#[napi(js_name = "SVGCanvas")]
+pub struct SVGCanvas {
+  pub width: u32,
+  pub height: u32,
+  pub(crate) ctx: ClassInstance<CanvasRenderingContext2D>,
+}
+
+#[napi]
+impl SVGCanvas {
+  #[napi(constructor)]
+  pub fn new(
+    mut env: Env,
+    mut this: This,
+    width: u32,
+    height: u32,
+    flag: SvgExportFlag,
+  ) -> Result<Self> {
+    let ctx = CanvasRenderingContext2D::into_instance(
+      CanvasRenderingContext2D {
+        context: Context::new_svg(width, height, flag.into(), ColorSpace::default())?,
+      },
+      env,
+    )?;
+    ctx.as_object(env).define_properties(&[
+      Property::new(FILL_STYLE_HIDDEN_NAME)?
+        .with_value(&env.create_string("#000")?)
+        .with_property_attributes(PropertyAttributes::Writable | PropertyAttributes::Configurable),
+      Property::new(STROKE_STYLE_HIDDEN_NAME)?
+        .with_value(&env.create_string("#000")?)
+        .with_property_attributes(PropertyAttributes::Writable | PropertyAttributes::Configurable),
+      Property::new("canvas")?
+        .with_value(&this)
+        .with_property_attributes(
+          PropertyAttributes::Default
+            | PropertyAttributes::Writable
+            | PropertyAttributes::Enumerable,
+        ),
+    ])?;
+    env.adjust_external_memory((width * height * 4) as i64)?;
+    this.define_properties(&[Property::new("ctx")?
+      .with_value(&ctx)
+      .with_property_attributes(PropertyAttributes::Default)])?;
+    Ok(Self { width, height, ctx })
+  }
+
+  #[napi]
+  pub fn get_context(
+    &mut self,
+    this: This,
+    context_type: String,
+    attrs: Option<CanvasRenderingContext2DAttributes>,
+  ) -> Result<Unknown> {
+    if context_type != "2d" {
+      return Err(Error::new(
+        Status::InvalidArg,
+        format!("{context_type} is not supported"),
+      ));
+    }
+    let context_2d = &mut self.ctx.context;
+    if !attrs.as_ref().and_then(|a| a.alpha).unwrap_or(true) {
+      let mut fill_paint = context_2d.fill_paint()?;
+      fill_paint.set_color(255, 255, 255, 255);
+      context_2d.alpha = false;
+      context_2d.surface.draw_rect(
+        0f32,
+        0f32,
+        self.width as f32,
+        self.height as f32,
+        &fill_paint,
+      );
+    }
+    let color_space = attrs
+      .and_then(|a| a.color_space)
+      .and_then(|cs| ColorSpace::from_str(&cs).ok())
+      .unwrap_or_default();
+    context_2d.color_space = color_space;
+    this.get_named_property("ctx")
+  }
+
+  #[napi]
+  pub fn get_content(&self, env: Env) -> Result<JsBuffer> {
+    let svg_data_stream = self.ctx.context.stream.as_ref().unwrap();
+    let svg_data = svg_data_stream.data(self.ctx.context.width, self.ctx.context.height);
+    unsafe {
+      env
+        .create_buffer_with_borrowed_data(svg_data.0.ptr, svg_data.0.size, svg_data, |d, _| {
+          mem::drop(d)
+        })
+        .map(|b| b.into_raw())
+    }
+  }
+}
+
+#[napi]
+pub fn clear_all_cache() {
+  unsafe { sk::ffi::skiac_clear_all_cache() };
 }
---
+++ src/path.rs
@@ -0,0 +1,398 @@
+use napi::{bindgen_prelude::*, JsString};
+
+use crate::sk::{
+  FillType as SkFillType, Matrix as SkMatrix, Path as SkPath, PathOp as SkPathOp,
+  StrokeCap as SkStrokeCap, StrokeJoin as SkStrokeJoin,
+};
+
+#[napi(object)]
+pub struct Matrix {
+  pub a: f64,
+  pub b: f64,
+  pub c: f64,
+  pub d: f64,
+  pub e: f64,
+  pub f: f64,
+}
+
+#[napi]
+pub enum PathOp {
+  Difference,        // subtract the op path from the first path
+  Intersect,         // intersect the two paths
+  Union,             // union (inclusive-or) the two paths
+  Xor,               // exclusive-or the two paths
+  ReverseDifference, // subtract the first path from the op path
+}
+
+impl From<PathOp> for SkPathOp {
+  fn from(value: PathOp) -> Self {
+    match value {
+      PathOp::Difference => SkPathOp::Difference,
+      PathOp::Intersect => SkPathOp::Intersect,
+      PathOp::Union => SkPathOp::Union,
+      PathOp::Xor => SkPathOp::Xor,
+      PathOp::ReverseDifference => SkPathOp::ReverseDifference,
+    }
+  }
+}
+
+#[napi]
+#[derive(PartialEq, Eq, Hash, Debug)]
+pub enum FillType {
+  Winding = 0,
+  EvenOdd = 1,
+  InverseWinding = 2,
+  InverseEvenOdd = 3,
+}
+
+impl From<FillType> for SkFillType {
+  fn from(value: FillType) -> Self {
+    match value {
+      FillType::Winding => SkFillType::Winding,
+      FillType::EvenOdd => SkFillType::EvenOdd,
+      FillType::InverseWinding => SkFillType::InverseWinding,
+      FillType::InverseEvenOdd => SkFillType::InverseEvenOdd,
+    }
+  }
+}
+
+impl From<i32> for FillType {
+  fn from(value: i32) -> Self {
+    match value {
+      0 => FillType::Winding,
+      1 => FillType::EvenOdd,
+      2 => FillType::InverseWinding,
+      3 => FillType::InverseEvenOdd,
+      _ => unreachable!(),
+    }
+  }
+}
+
+#[napi]
+#[derive(Default)]
+pub enum StrokeCap {
+  #[default]
+  Butt = 0,
+  Round = 1,
+  Square = 2,
+}
+
+impl From<StrokeCap> for SkStrokeCap {
+  fn from(value: StrokeCap) -> Self {
+    match value {
+      StrokeCap::Butt => SkStrokeCap::Butt,
+      StrokeCap::Round => SkStrokeCap::Round,
+      StrokeCap::Square => SkStrokeCap::Square,
+    }
+  }
+}
+
+#[napi]
+#[derive(Default)]
+pub enum StrokeJoin {
+  #[default]
+  Miter = 0,
+  Round = 1,
+  Bevel = 2,
+}
+
+impl From<StrokeJoin> for SkStrokeJoin {
+  fn from(value: StrokeJoin) -> Self {
+    match value {
+      StrokeJoin::Miter => SkStrokeJoin::Miter,
+      StrokeJoin::Round => SkStrokeJoin::Round,
+      StrokeJoin::Bevel => SkStrokeJoin::Bevel,
+    }
+  }
+}
+
+#[napi(object)]
+pub struct StrokeOptions {
+  pub width: Option<f64>,
+  pub miter_limit: Option<f64>,
+  pub cap: Option<StrokeCap>,
+  pub join: Option<StrokeJoin>,
+}
+
+#[napi]
+pub struct Path {
+  pub(crate) inner: SkPath,
+}
+
+#[napi]
+impl Path {
+  #[napi(constructor)]
+  pub fn new(path: Option<Either3<String, &mut Path, Unknown>>) -> Result<Self> {
+    let inner = match &path {
+      Some(Either3::A(path)) => SkPath::from_svg_path(path).ok_or_else(|| {
+        Error::new(
+          Status::InvalidArg,
+          "Create path from provided path string failed.".to_owned(),
+        )
+      })?,
+      Some(Either3::B(path)) => path.inner.clone(),
+      Some(Either3::C(c)) => {
+        return Err(Error::new(
+          Status::InvalidArg,
+          format!(
+            "Create path from provided unknown value failed {}.",
+            c.get_type()?
+          ),
+        ));
+      }
+      None => SkPath::new(),
+    };
+    Ok(Path { inner })
+  }
+
+  #[napi]
+  pub fn add_path(&mut self, sub_path: &Path, matrix: Option<Matrix>) {
+    let transform = matrix
+      .map(|m| {
+        SkMatrix::new(
+          m.a as f32, m.c as f32, m.e as f32, m.b as f32, m.d as f32, m.f as f32,
+        )
+      })
+      .unwrap_or_else(SkMatrix::identity);
+    self.inner.add_path(&sub_path.inner, &transform);
+  }
+
+  #[napi]
+  pub fn close_path(&mut self) {
+    self.inner.close();
+  }
+
+  #[napi]
+  pub fn move_to(&mut self, x: f64, y: f64) {
+    self.inner.move_to(x as f32, y as f32);
+  }
+
+  #[napi]
+  pub fn line_to(&mut self, x: f64, y: f64) {
+    self.inner.line_to(x as f32, y as f32);
+  }
+
+  #[napi]
+  pub fn bezier_curve_to(&mut self, cp1x: f64, cp1y: f64, cp2x: f64, cp2y: f64, x: f64, y: f64) {
+    self.inner.cubic_to(
+      cp1x as f32,
+      cp1y as f32,
+      cp2x as f32,
+      cp2y as f32,
+      x as f32,
+      y as f32,
+    );
+  }
+
+  #[napi]
+  pub fn quadratic_curve_to(&mut self, cpx: f64, cpy: f64, x: f64, y: f64) {
+    self
+      .inner
+      .quad_to(cpx as f32, cpy as f32, x as f32, y as f32);
+  }
+
+  #[napi]
+  pub fn arc(
+    &mut self,
+    x: f64,
+    y: f64,
+    radius: f64,
+    start_angle: f64,
+    end_angle: f64,
+    anticlockwise: Option<bool>,
+  ) {
+    self.inner.arc(
+      x as f32,
+      y as f32,
+      radius as f32,
+      start_angle as f32,
+      end_angle as f32,
+      anticlockwise.unwrap_or(false),
+    );
+  }
+
+  #[napi]
+  pub fn arc_to(&mut self, x1: f64, y1: f64, x2: f64, y2: f64, radius: f64) {
+    self
+      .inner
+      .arc_to_tangent(x1 as f32, y1 as f32, x2 as f32, y2 as f32, radius as f32);
+  }
+
+  #[napi]
+  pub fn ellipse(
+    &mut self,
+    x: f64,
+    y: f64,
+    radius_x: f64,
+    radius_y: f64,
+    rotation: f64,
+    start_angle: f64,
+    end_angle: f64,
+    anticlockwise: Option<bool>,
+  ) {
+    self.inner.ellipse(
+      x as f32,
+      y as f32,
+      radius_x as f32,
+      radius_y as f32,
+      rotation as f32,
+      start_angle as f32,
+      end_angle as f32,
+      anticlockwise.unwrap_or(false),
+    );
+  }
+
+  #[napi]
+  pub fn rect(&mut self, x: f64, y: f64, width: f64, height: f64) {
+    self
+      .inner
+      .add_rect(x as f32, y as f32, width as f32, height as f32);
+  }
+
+  #[napi]
+  pub fn round_rect(
+    &mut self,
+    x: f64,
+    y: f64,
+    width: f64,
+    height: f64,
+    radii: Either3<f64, Vec<f64>, Undefined>,
+  ) {
+    let radii_array: [f32; 4] = match radii {
+      Either3::A(radii) => [radii as f32; 4],
+      Either3::B(radii_vec) => match radii_vec.len() {
+        0 => [0f32; 4],
+        1 => [radii_vec[0] as f32; 4],
+        2 => [
+          radii_vec[0] as f32,
+          radii_vec[1] as f32,
+          radii_vec[0] as f32,
+          radii_vec[1] as f32,
+        ],
+        3 => [
+          radii_vec[0] as f32,
+          radii_vec[1] as f32,
+          radii_vec[1] as f32,
+          radii_vec[2] as f32,
+        ],
+        _ => [
+          radii_vec[0] as f32,
+          radii_vec[1] as f32,
+          radii_vec[2] as f32,
+          radii_vec[3] as f32,
+        ],
+      },
+      Either3::C(_) => [0f32; 4],
+    };
+    self
+      .inner
+      .round_rect(x as f32, y as f32, width as f32, height as f32, radii_array);
+  }
+
+  #[napi]
+  pub fn op(&mut self, other: &Path, op: PathOp) -> &Self {
+    self.inner.op(&other.inner, op.into());
+    self
+  }
+
+  #[napi(js_name = "toSVGString")]
+  pub fn to_svg_string(&self, env: Env) -> Result<JsString> {
+    let sk_string = self.inner.to_svg_string();
+    unsafe { env.create_string_from_c_char(sk_string.ptr, sk_string.length) }
+  }
+
+  #[napi]
+  pub fn simplify(&mut self) -> &Self {
+    self.inner.simplify();
+    self
+  }
+
+  #[napi]
+  pub fn set_fill_type(&mut self, fill_type: FillType) {
+    self.inner.set_fill_type(fill_type.into());
+  }
+
+  #[napi]
+  pub fn get_fill_type(&mut self) -> FillType {
+    self.inner.get_fill_type().into()
+  }
+
+  #[napi]
+  pub fn get_fill_type_string(&mut self) -> String {
+    match self.get_fill_type() {
+      FillType::EvenOdd => "evenodd".to_owned(),
+      _ => "nonzero".to_owned(),
+    }
+  }
+
+  #[napi]
+  pub fn as_winding(&mut self) -> &Self {
+    self.inner.as_winding();
+    self
+  }
+
+  #[napi]
+  pub fn stroke(&mut self, options: StrokeOptions) -> &Self {
+    self.inner.stroke(
+      options.cap.unwrap_or_default().into(),
+      options.join.unwrap_or_default().into(),
+      options.width.unwrap_or(1.0) as f32,
+      options.miter_limit.unwrap_or(4.0) as f32,
+    );
+    self
+  }
+
+  #[napi]
+  pub fn compute_tight_bounds(&self) -> Vec<f64> {
+    let ltrb = self.inner.compute_tight_bounds();
+    vec![ltrb.0 as f64, ltrb.1 as f64, ltrb.2 as f64, ltrb.3 as f64]
+  }
+
+  #[napi]
+  pub fn get_bounds(&self) -> Vec<f64> {
+    let ltrb = self.inner.get_bounds();
+    vec![ltrb.0 as f64, ltrb.1 as f64, ltrb.2 as f64, ltrb.3 as f64]
+  }
+
+  #[napi]
+  pub fn transform(&mut self, matrix: Matrix) -> &Self {
+    let trans = SkMatrix::new(
+      matrix.a as f32,
+      matrix.c as f32,
+      matrix.e as f32,
+      matrix.b as f32,
+      matrix.d as f32,
+      matrix.f as f32,
+    );
+    self.inner.transform_self(&trans);
+    self
+  }
+
+  #[napi]
+  pub fn trim(&mut self, start: f64, end: f64, is_complement: Option<bool>) -> &Self {
+    self
+      .inner
+      .trim(start as f32, end as f32, is_complement.unwrap_or(false));
+    self
+  }
+
+  #[napi]
+  pub fn dash(&mut self, on: f64, off: f64, phase: f64) -> &Self {
+    self.inner.dash(on as f32, off as f32, phase as f32);
+    self
+  }
+
+  #[napi]
+  pub fn equals(&self, other: &Path) -> bool {
+    self.inner == other.inner
+  }
+
+  #[napi]
+  pub fn is_point_in_path(&self, x: f64, y: f64, fill_type: Option<FillType>) -> bool {
+    self.inner.hit_test(
+      x as f32,
+      y as f32,
+      fill_type.unwrap_or(FillType::Winding).into(),
+    )
+  }
+}
